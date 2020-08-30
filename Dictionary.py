6c85a3b362636ca99284739ceb5d318701a737e7
RadzJoshi27 <70031755+RadzJoshi27@users.noreply.github.com>
2020-08-30 21:58:09 +0530

    Dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -1,6 +1,11 @@
 fruits={1:'Apple',2:'Blueberry',3:'Orange',4:'Kiwi',5:'Melon',6:'Watermelon'}
 print(fruits)
+#{1: 'Apple', 2: 'Blueberry', 3: 'Orange', 4: 'Kiwi', 5: 'Melon', 6: 'Watermelon'}
 print('Fruit having orange color is ' + fruits[3] + '.')
+#Fruit having orange color is Orange.
 print('My favourite fruit is ' + fruits[2] + '.')
+#My favourite fruit is Blueberry.
 print(fruits.keys())
+#dict_keys([1, 2, 3, 4, 5, 6])
 print(fruits.values())
+#dict_values(['Apple', 'Blueberry', 'Orange', 'Kiwi', 'Melon', 'Watermelon'])
